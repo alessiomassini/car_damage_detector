55bbe2e89016b8fcc7434398a2770725c3a5a17a
gianlucaregoli <74818952+gianlucaregoli@users.noreply.github.com>
2020-11-26 20:15:05 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 os.chdir(os.getcwd())
 os.system('git clone "https://github.com/AlexeyAB/darknet.git"')
 
-shutil.move(os.getcwd()+"./detector.py",os.getcwd()+"/darknet/detector.py")
-shutil.move(os.getcwd()+"./obj.data",os.getcwd()+"/darknet/data/obj.data")
-shutil.move(os.getcwd()+"./obj.names",os.getcwd()+"/darknet/data/obj.names")
-shutil.move(os.getcwd()+"./yolov4-obj.cfg",os.getcwd()+"/darknet/cfg/yolov4-obj.cfg")
+shutil.move(os.getcwd()+"/detector.py",os.getcwd()+"/darknet/detector.py")
+shutil.move(os.getcwd()+"/obj.data",os.getcwd()+"/darknet/data/obj.data")
+shutil.move(os.getcwd()+"/obj.names",os.getcwd()+"/darknet/data/obj.names")
+shutil.move(os.getcwd()+"/yolov4-obj.cfg",os.getcwd()+"/darknet/cfg/yolov4-obj.cfg")
 
 os.chdir('./darknet')
 gdd.download_file_from_google_drive(file_id='1ebQ1ffbaX4LMCFgVnjpfFnsbIDm8Q_IU',
